c447391c1dc171ea7c8170d505ec6c65c6224be7
Flynn <flynnigan.tan@gmail.com>
2017-08-03 15:44:10 -0500

    added routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,6 +3,8 @@ var express = require("express");
 
 var router = express.Router();
 
+var db = require("../models");
+
 // Router -----------------------------------------------------
 
 // create routes that will connect the model and view
@@ -10,17 +12,40 @@ var router = express.Router();
 // route that gets/shows all the burgers
 router.get("/", function(req,res){
 	
+	db.Burger.findAll({}).then(function(dbBurger){
+
+		var burgerObject = {
+			burgers: dbBurger
+		};
+
+		res.render("index, burgerObject");
+	});
 });
 
 // route to post/add a burger
 router.post("/", function(req,res){
 
-	
+	db.Burger.create({
+		burger_name:req.body.burger_name
+	}).then(function(dbBurger){
+
+		res.redirect("/");
+	});
 });
 
 // route to devour a burger by updating its devoured category to true
 router.put("/:id", function(req,res){
 	
+	db.Burger.update({
+		devoured:true
+	},{
+		where:{
+			id:req.params.id
+		}
+	}).then(function(dbBurger){
+
+		res.redirect("/");
+	});
 });
 
 module.exports = router;
